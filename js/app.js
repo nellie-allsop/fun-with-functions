0042de4581cdcc9f1c2300164c08101049e6d4fe
Nellie Allsop <nellie.allsop@gmail.com>
2023-09-22 12:39:09 +0100

    Finish lab

--- js/app.js
+++ js/app.js
@@ -55,11 +55,26 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
 	//eslint-disable-line
-	let result = a + b + c;
+	let resultOne = sum(a,b)[0];
+	let resultTwo = sum(resultOne,c)[0];
+	let resultThree = multiply(a,b)[0];
+	let resultFour = multiply(resultThree,c)[0];
+	let messageOne = a + " and " + b + " and " + c + " sum to " + resultTwo + ".";
+	let messageTwo =
+		"The product of " +
+		a +
+		" and " +
+		b +
+		" and " +
+		c +
+		" is " +
+		resultFour +
+		".";
+	return [resultTwo, resultFour, messageOne, messageTwo];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -123,10 +138,21 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
+
+//Started completing 6 as Sam typed it in during the demo (don't understand it, just want it here so that one day I can hopefully answer it!)
+
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
 	//eslint-disable-line
+  let product = 1;
+
+  for (let i = 0; i < dynamicArray.length; i++){
+    product = multiply(product,dynamicArray[i])[0]
+  }
+
+  let string = 'The numbers ' ${dynamicArray} have a product of $product
+  return [product, string]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
