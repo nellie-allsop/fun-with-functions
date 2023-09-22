524d955a5b162f5e91a782dc31a77263fe2ca007
Nellie Allsop <nellie.allsop@gmail.com>
2023-09-22 10:37:51 +0100

    Answer Q2

--- js/app.js
+++ js/app.js
@@ -31,10 +31,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) {
 	//eslint-disable-line
+	let result = a * b;
+	let message = "The product of " + a + " and " + b + " is " + result + ".";
+	return [result, message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,6 +55,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
 	//eslint-disable-line
+	let result = a + b + c;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
